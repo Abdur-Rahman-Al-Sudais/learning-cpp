7d318cfc3146c0e4c8c15619e6f0ecffe2ed79f2
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-06-26 14:50:07 -0700

    playing with namespaces in c++

---
+++ namespaces.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// using namespace std;
+
+// using std::cout;
+// using std::endl;
+
+namespace customNamespace{
+    void display(){
+        // I can create my own namespace.
+    }
+}
+
+int main(){
+    std::cout << "Hello world 1" << std::endl;
+    std::cout << "Hello world 2" << std::endl;
+    return 0;
+}
