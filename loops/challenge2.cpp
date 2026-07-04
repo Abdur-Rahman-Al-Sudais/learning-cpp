ce3bfdb8f1c7a524cdbf0281b8cf95d5a48c19d0
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-04 09:34:24 -0700

    loops/challenge 2 done

---
+++ loops/challenge2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string response;
+    
+    do {
+        cout << "Do you want tea(yes/no): ";
+        getline(cin, response);
+    } while (response != "no");
+
+
+    return 0;
+}
