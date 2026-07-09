ba48275b24a13f7d5cbeb814242a707f88aadfb8
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-09 11:45:10 -0700

    learn array with indexes

---
+++ arrays/array_with_index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int evenNumbers[5] = {2, 32, 16, 88, 100};
+
+    cout << evenNumbers[0] << endl;
+    cout << evenNumbers[1] << endl;
+    cout << evenNumbers[2] << endl;
+    cout << evenNumbers[3] << endl;
+    cout << evenNumbers[4] << endl;
+
+    return 0;
+}
