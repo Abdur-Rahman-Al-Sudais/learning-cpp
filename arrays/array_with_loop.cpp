0c0be8e521996e15c23e16ac09e18d797addea73
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-09 11:48:42 -0700

    learn array with loop

---
+++ arrays/array_with_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int evenNumbers[5] = {2, 32, 16, 88, 100};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << evenNumbers[i] << endl;
+    }
+
+    return 0;
+}
